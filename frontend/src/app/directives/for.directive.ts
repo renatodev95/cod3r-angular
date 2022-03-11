2cf23f806d24d438cde61716eb4b39cb35685508
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-03-11 09:48:12 -0300

    aplicando Diretiva Estrutural

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,21 @@
+import { Input } from '@angular/core';
+import { TemplateRef } from '@angular/core';
+import { ViewContainerRef } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  @Input('myForEm') numbers: number[]
+
+  constructor(private container: ViewContainerRef, private template: TemplateRef<any>) { }
+
+  ngOnInit(): void {
+    for (let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number });
+    }
+  }
+}
