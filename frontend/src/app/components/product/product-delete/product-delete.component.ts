1ffeb1de28a00f727b6f69891f325f8e2e6252aa
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-03-15 09:58:49 -0300

    ajustes no css e componente de exclusao

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -27,7 +27,7 @@ export class ProductDeleteComponent implements OnInit {
 
   deleteProduct(): void {
     this.productService.delete(this.product.id).subscribe(() => {
-      this.productService.showMessage("Produto removido com sucesso!");
+      this.productService.showMessage("Produto excluido com sucesso!");
       this.router.navigate(["/products"]);
     });
   }
