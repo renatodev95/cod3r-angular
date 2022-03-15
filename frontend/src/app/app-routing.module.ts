fe4179f42d3841ee84c3ba415b6c6a4140141c3e
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-03-15 09:48:24 -0300

    completando o desafio: criando o componente para excluir produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,31 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
+import { ProductDeleteComponent } from "./components/product/product-delete/product-delete.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
-  }, {
+  },
+  {
     path: "products",
-    component: ProductCrudComponent
+    component: ProductCrudComponent,
   },
   {
     path: "products/create",
-    component: ProductCreateComponent
+    component: ProductCreateComponent,
   },
   {
     path: "products/update/:id", // a sintaxe ':id' indica que o id se trata de um parâmetro e descarta os : (dois pontos)
-    component: ProductUpdateComponent
-  }
+    component: ProductUpdateComponent,
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], 
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
